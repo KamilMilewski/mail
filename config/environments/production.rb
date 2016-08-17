d41515d7f8ef38451bc2ba5d3b6226902a110879
Kamil <kamil.milewski@hotmail.com>
2016-08-17 19:53:50 +0200

    updated production config for gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,17 +83,17 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'poczta.o2.pl',
+    address: 'smtp.gmail.com',
     port: 587,
     domain: 'example.com',
-    user_name: ENV['username'],
-    password: ENV['password'],
+    user_name: ENV['gmail_username'],
+    password: ENV['gmail_password'],
     authentication: 'plain',
     enable_starttls_auto: true
   }
